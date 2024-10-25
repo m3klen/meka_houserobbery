d6d078f0746b3e2db5b82a619691eed86439f316
m3klen <fivemrex9873@gmail.com>
2024-10-25 19:24:29 +0200

    Locales files

---
+++ locales/locales.lua
@@ -0,0 +1,56 @@
+Locales = {}
+
+Locales["da"] = {
+    ["rob_house"] = "Røv Hus",
+    ["enter_house"] = "Gå ind i hus",
+    ["exit_house"] = "Forlad Hus",
+    ["leaving_house"] = "Forlader husset",
+
+    ["search_bed"] = "Led i sengen",
+    ["search_fridge"] = "Led i køleskabbet",
+    ["search_storage"] = "Led i opbevaringen",
+    ["search_table"] = "Led på bordet",
+    ["search_tv"] = "Led på TV staviet",
+    ["search_bedside"] = "Led på senge bordet",
+
+    ["search_painting"] = "Stjæl Maleri",
+
+    -- Labels
+    ["searching_bed"] = "Leder i sengen",
+    ["searching_fridge"] = "Leder i køleskabbet",
+    ["searching_storage"] = "Leder i opbevaringen",
+    ["searching_table"] = "Leder på bordet",
+    ["searching_tv"] = "Leder på TV staviet",
+    ["searching_bedside"] = "Leder på senge bordet",
+    
+
+    ["searched_bed"] = "Du har ledt i sengen!",
+    ["searched_fridge"] = "Du har ledt i køleskabbet!",
+    ["searched_storage"] = "Du har ledt i opbevaringen!",
+    ["searched_table"] = "Du har ledt på bordet!",
+    ["searched_tv"] = "Du har ledt på TV staviet!",
+    ["searched_bedside"] = "Du har ledt på senge bordet!",
+
+    -- Notifications
+    ["not_enough_lockpicks"] = "Du har ikke nok lockpicks!",
+
+    ["broke_in"] = "Du kom ind i huset!",
+    ["failed_brake_in"] = "Du fejlede med at lockpicke!",
+
+    ['left_house'] = "Du forlod huset!",
+    ['cancel_action'] = "Du annullerede aktionen!",
+
+    ['found_message'] = "Du har fundet ",
+    ['nofound_message'] = "Du fandt ikke noget!",
+
+    ["lockpick_broke"] = "Din lockpick gik i stykker!",
+
+    ["found_something"] = "Du fandt noget!",
+    ["not_enough_inv_place"] = "Du har ikke nok plads i dit inventar!",
+    ["found_nothing"] = "Du fandt ikke noget",
+}
+
+Locales["en"] = {
+    ["rob_house"] = "Rob House",
+    ["enter_house"] = "Enter House",
+}
